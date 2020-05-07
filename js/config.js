475477e3ec5d0ee12a3074b9e9fe1af4249e0f83
aricardo2020 <64984925+aricardo2020@users.noreply.github.com>
2020-05-07 20:23:12 +0100

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_8yRyI7ZU7', 
+        userPoolClientId: '37jhluj5i8vcapsarivinrjcdp', 
+        region: 'us-east-1'
+    },
+    api: {
+        invokeUrl: 'https://5bxnzr7f7l.execute-api.us-east-1.amazonaws.com/prod'
+    }
+};
